42499271db5d2d50e1570c86f9540e4fafa9c7bd
tianpc0318 <tianpc0318@163.com>
2017-03-26 19:08:01 +0800

    rocketmq和redis分布式锁

--- src/test/java/com/vlike/test/CommonTest.java
+++ src/test/java/com/vlike/test/CommonTest.java
@@ -1,7 +1,9 @@
 package com.vlike.test;
 
 import org.apache.commons.codec.binary.Base64;
-
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.StringUtils;
+	
 public class CommonTest {
 
 	public static void main(String[] args) {
@@ -10,6 +12,10 @@ public class CommonTest {
 		String target = new String(result);
 		System.out.println(target);
 		System.out.println(new String(Base64.decodeBase64(target)));
+		String[] arrs = StringUtils.tokenizeToStringArray("tianpc;peng,cheng;dream", ConfigurableApplicationContext.CONFIG_LOCATION_DELIMITERS);
+		for(String arr : arrs) {
+			System.out.println(arr);
+		}
 	}
 
 }
